04ed9b57cf7614fe897ec7003edfcd82d1ba09bd
micheleferri01 <micheleferri095@gmail.com>
2026-01-12 15:35:46 +0100

    aggiunto modifica del contenuto dell'attributo alt al cambio di stato

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ const interruttore = document.getElementById("interruttore");
 interruttore.addEventListener("click", lightSwitch);
 
 function lightSwitch () {
-    if ((interruttore.innerText === "Accendi") && (lampada.src.includes("white_lamp.png"))){
+    if ((interruttore.innerText.includes("Accendi")) && (lampada.src.includes("white_lamp.png"))){
         lampada.src = "./img/yellow_lamp.png";
+        lampada.alt = "luce-accesa";
         interruttore.innerText = "Spegni";
     }
     else{
         lampada.src = "./img/white_lamp.png";
+        lampada.alt = "lampada-spenta";
         interruttore.innerText = "Accendi";
     }
 }
